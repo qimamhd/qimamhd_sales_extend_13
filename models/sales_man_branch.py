eb8e1f7c7ae17ed766c7d9c311f80b7cc5f86cab
Mohammed Ali <bamalliss84@gmail.com>
2023-06-03 23:27:06 +0300

    update

--- models/sales_man_branch.py
+++ models/sales_man_branch.py
@@ -8,3 +8,5 @@ class inherit_sales_mans(models.Model):
 
     branch_id = fields.Many2one('custom.branches', required=True, string="الفرع", default=lambda self: self.env.user.branch_id.id)
 
+    sale_user = fields.Many2one('res.users', string='مستخدم المبيعات', index=True, track_visibility='onchange',
+                                  track_sequence=2)
